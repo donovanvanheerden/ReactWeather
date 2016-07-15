3a6dcd70d49a5f300cd55e5769e501e2eeb5abb8
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-07-15 14:46:54 +0200

    updated nav styling

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,14 +1,22 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
+var navStyle = {
+  color: 'rgba(255,255,255,0.7)'
+}
+
+var activeStyle = {
+  color: 'white'
+}
+
 var Nav = (props) => {
   return (
     <nav>
       <div class="nav-wrapper">
         <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-          <li><IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink></li>
-          <li><IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink></li>
+          <li><IndexLink to="/" activeClassName="active" style={navStyle} activeStyle={activeStyle}>Get Weather</IndexLink></li>
+          <li><IndexLink to="/about" activeClassName="active" style={navStyle} activeStyle={activeStyle}>About</IndexLink></li>
+          <li><IndexLink to="/examples" activeClassName="active" style={navStyle} activeStyle={activeStyle}>Examples</IndexLink></li>
         </ul>
       </div>
     </nav>
