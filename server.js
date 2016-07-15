67824875783ac95b542b4a20d92233686610561d
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-07-15 14:57:10 +0200

    altered server for localhost run

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) { // reqest, response, next
-  if (req.headers['x-forwarded-proto'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'http' || req.hostname === 'localhost') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
