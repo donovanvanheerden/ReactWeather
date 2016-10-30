5d635083c77072b4871355d6f8fb7ec98adcee3d
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-10-30 07:55:12 +0200

    update nav component

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -5,9 +5,9 @@ var Nav = (props) => {
   return (
     <div>
       <h2>Nav Component</h2>
-        <IndexLink to="/" activeClassName="active" style={navStyle} activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" style={navStyle} activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" style={navStyle} activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
     </div>
   );
 }
