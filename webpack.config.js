2dfe8cb80d1bf331108fb97bbdc51b61847aaa58
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-10-30 08:16:14 +0200

    added foundation + additional modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx',
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -10,10 +25,10 @@ module.exports = {
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Navigation.jsx',
       Weather: 'app/components/Weather.jsx',
-      Examples: 'app/components/Examples.jsx',
-      WeatherResult: 'app/components/WeatherResult.jsx',
       WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherResult: 'app/components/WeatherResult.jsx',
       About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx'
     },
     extensions: ['', '.js', '.jsx']
