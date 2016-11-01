b7138ff1e949272b12ff2a6383c317102648d064
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-11-01 20:53:11 +0200

    updated styles

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="location" placeholder="Enter City Name"/>
+          <input type="text" ref="location" placeholder="Search Weather"/>
           <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
